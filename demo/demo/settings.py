a7aa784f965083dd1f905826176fea2607425c96
Medi Assumani <mediassumani49@gmail.com>
2019-06-03 13:23:23 -0700

    [Add] activated models

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin', # the admin dashboard stuff
     'django.contrib.auth', # authentification system, no need to implement auth
     'django.contrib.contenttypes', # framework for content types
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'PDT'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
--- demo/polls/models.py
+++ demo/polls/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model): # sublassing the models.Model class
-    question_text = models.CharField(max_length=-20)
-    pub_date = models.DateTimeField('date published')
+
+    # each property represents a database field
+    # each field is represented by an instance of the Field
+    question_text = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('date published') # first argument is an optional for human readable
 
 
 class Choice(models.Model):
+    # ForeignKey is used to defined a relationship with a different model
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
