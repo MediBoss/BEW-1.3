6587bc21269a43621ad93cfa1f41bfadbc33fab0
Medi Assumani <mediassumani49@gmail.com>
2019-06-06 09:24:50 -0700

    [Update] working on views

--- demo/polls/urls.py
+++ demo/polls/urls.py
@@ -3,5 +3,12 @@ from . import views # importing the views from the current directory
 
 # URL configuration
 urlpatterns = [ 
-    path('', views.index, name="index")
+    # endpoint for home page
+    path('', views.index, name="index"),
+    # endpoint for question detail
+    path('<int:question_id>/', views.detail, name='detail'),
+    # endpoint for question resuts
+    path('<int:question_id>/results/', views.results, name='results'),
+    # endpoint for question votes
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- demo/polls/views.py
+++ demo/polls/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello Medi, You're at Polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+# View(controller) to handle the page that shows detail of a question
+def detail(request, question_id):
+    return HttpResponse("You're Looking at question %s." % question_id)
+# View(controller) to handle the page that shows results of of a question
+def results(request, question_id):
+
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response, question_id)
+
+# View(controller) to handle the page that shows the vote of a question
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
 
